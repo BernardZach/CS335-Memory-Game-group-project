8e1df09303f3e9efcc87ccd69cab42e29a081a46
BernardZach <bernardzach00@gmail.com>
2023-11-16 13:55:59 -0500

    created 2 image at a time functionality

--- qtTesting.py
+++ qtTesting.py
@@ -32,6 +32,12 @@ class ImageChangerApp(QMainWindow):
         # Create a list to hold references to the image labels
         self.image_labels = []
 
+        # Create a list to hold the positions and assigned images
+        self.card_positions = []
+
+        # Create a list to keep track of flipped cards
+        self.flipped_cards = []
+
         # Load the initial image for all squares (question-mark.png)
         for row in range(4):
             for col in range(4):
@@ -40,13 +46,13 @@ class ImageChangerApp(QMainWindow):
                 self.load_image(image_label, "assets/question-mark.png")
                 self.image_labels.append(image_label)
 
+                # Store the position and assigned image in the list
+                self.card_positions.append(((row, col), image_files.pop()))
+
                 # Connect the clicked signal to the change_image function for each label
                 image_label.mousePressEvent = lambda event, row=row, col=col: self.change_image(event, row, col)
 
                 self.layout.addWidget(image_label, row, col)
-
-        # Store the randomly assigned images for each card
-        self.randomly_assigned_images = list(image_files)
 
     def load_image(self, label, image_path):
         pixmap = QPixmap(image_path)
@@ -56,13 +62,29 @@ class ImageChangerApp(QMainWindow):
     def change_image(self, event, row, col):
         # Check if the left mouse button is clicked
         if event.button() == Qt.LeftButton:
-            # Change the image of the clicked square to the randomly assigned one
+            # Find the position of the clicked square in the card_positions list
             index = row * 4 + col
-            image_path = os.path.join("assets", self.randomly_assigned_images[index])
+            position, assigned_image = self.card_positions[index]
+
+            # Check if the maximum number of flipped cards (2) is reached
+            if len(self.flipped_cards) == 2:
+                # Flip back the first card in the list
+                first_card_index = self.flipped_cards.pop(0)
+                self.flip_card_back(first_card_index)
+
+            # Change the image of the clicked square to the assigned one
+            image_path = os.path.join("assets", assigned_image)
             self.load_image(self.image_labels[index], image_path)
 
-            # Print the clicked card index (You can modify this part as needed)
-            print(f"Card clicked: {index}")
+            # Keep track of the flipped card index
+            self.flipped_cards.append(index)
+
+            # Print the clicked card position and assigned image (Modify this part as needed)
+            print(f"Card clicked: Position={position}, Assigned Image={assigned_image}")
+
+    def flip_card_back(self, index):
+        # Flip back the card at the given index to the initial image
+        self.load_image(self.image_labels[index], "assets/question-mark.png")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
